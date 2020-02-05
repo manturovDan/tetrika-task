0633dec5de9bb732c59c29a720fe763c955c786c
manturovDan <manturov.dan@mail.ru>
2020-02-05 20:23:27 +0300

    getting array

--- task1/main.py
+++ task1/main.py
@@ -1,1 +1,12 @@
-#test
+def get_sort_lst_from_file(filename):
+    lst = open(filename, "r").readline().replace("\"", "").split(",")
+    lst.sort()
+    return lst
+
+def get_sum(names_lst):
+    esum = 0
+    for ina, na in enumerate(names_lst):
+        print(na)
+
+if __name__ == "__main__":
+    get_sort_lst_from_file("names.txt")
